874cc93a5699d3a40a1e2a1302bcb2734dbd0769
Павел <raevsky3@gmail.com>
2021-02-18 11:25:29 +0200

    Подключает leaflet карту с ошибкой

--- js/form.js
+++ js/form.js
@@ -1,28 +1,71 @@
 import {offersLabelsMap} from './data.js';
 
-const propertyTypeElement = document.querySelector('#type');
-const priceForNightElement = document.querySelector('#price');
+const mapFilterElement = document.querySelector('.map__filters');
+const typeFilterElement = mapFilterElement.querySelector('#housing-type');
+const priceFilterElement = mapFilterElement.querySelector('#housing-price');
+const roomsFilterElement = mapFilterElement.querySelector('#housing-rooms');
+const guestsFilterElement = mapFilterElement.querySelector('#housing-guests');
+const featuresFilterElement = mapFilterElement.querySelector('#housing-features');
+
+
+const adFormElement = document.querySelector('.ad-form');
+const adFormHeaderElement = adFormElement.querySelector('.ad-form-header');
+const titleFormElement = adFormElement.querySelector('#title');
+const addressFormElement = adFormElement.querySelector('#address');
+const propertyTypeFormElement = adFormElement.querySelector('#type');
+const priceFormElement = adFormElement.querySelector('#price');
+const timeInFormElement = adFormElement.querySelector('#timein');
+const timeOutFormElement = adFormElement.querySelector('#timeout');
+const roomNumberFormElement = adFormElement.querySelector('#room_number');
+const capacityRoomFormElement = adFormElement.querySelector('#capacity');
+const featuresFormElement = adFormElement.querySelector('.features');
+const descriptionFormElement = adFormElement.querySelector('#description');
+const imagesFormElement = adFormElement.querySelector('#images');
+const submitFormElement = adFormElement.querySelector('.ad-form__element--submit');
 
 const selectChangeTypeHandler = () => {
-  const offerType = propertyTypeElement.value;
+  const offerType = propertyTypeFormElement.value;
   const offerPriceLabel = offersLabelsMap[offerType].price;
-  priceForNightElement.setAttribute('min', offerPriceLabel);
-  priceForNightElement.setAttribute('placeholder', offerPriceLabel);
+  priceFormElement.setAttribute('min', offerPriceLabel);
+  priceFormElement.setAttribute('placeholder', offerPriceLabel);
 };
 
-propertyTypeElement.addEventListener('change', selectChangeTypeHandler);
-
-const timeInElement = document.querySelector('#timein');
-const timeOutElement = document.querySelector('#timeout');
+propertyTypeFormElement.addEventListener('change', selectChangeTypeHandler);
 
 const selectChangeTimeHandler = (firstTimeElement, secondTimeElement) => {
   firstTimeElement.value = secondTimeElement.value;
 };
 
-timeInElement.addEventListener('change', () => {
-  return selectChangeTimeHandler(timeOutElement, timeInElement);
+timeInFormElement.addEventListener('change', () => {
+  return selectChangeTimeHandler(timeOutFormElement, timeInFormElement);
 });
 
-timeOutElement.addEventListener('change', () => {
-  return selectChangeTimeHandler(timeInElement, timeOutElement);
+timeOutFormElement.addEventListener('change', () => {
+  return selectChangeTimeHandler(timeInFormElement, timeOutFormElement);
 });
+
+const makesInactiveFormHandler = () => {
+  mapFilterElement.classList.add('map__filters--disabled');
+  typeFilterElement.disabled = true;
+  priceFilterElement.disabled = true;
+  roomsFilterElement.disabled = true;
+  guestsFilterElement.disabled = true;
+  featuresFilterElement.disabled = true;
+
+  adFormElement.classList.add('ad-form--disabled');
+  titleFormElement.disabled = true;
+  adFormHeaderElement.disabled = true;
+  addressFormElement.disabled = true;
+  propertyTypeFormElement.disabled = true;
+  priceFormElement.disabled = true;
+  timeInFormElement.disabled = true;
+  timeOutFormElement.disabled = true;
+  roomNumberFormElement.disabled = true;
+  capacityRoomFormElement.disabled = true;
+  featuresFormElement.disabled = true;
+  descriptionFormElement.disabled = true;
+  imagesFormElement.disabled = true;
+  submitFormElement.disabled = true;
+};
+
+document.addEventListener('DOMContentLoaded', makesInactiveFormHandler);
--- js/map.js
+++ js/map.js
@@ -1,6 +1,12 @@
-import {createAd} from './data.js';
-import {createSimilarAd} from './popup.js';
+const map = L.map('#map__canvas')
+  .setView({
+    lat: 59.92749,
+    lng: 30.31127,
+  }, 10);
 
-const map = document.querySelector('.map__canvas');
-const similarAd = createSimilarAd(createAd());
-map.appendChild(similarAd);
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
