f562f70909b77915ceb01a43376ceb027be9cddb
Павел <raevsky3@gmail.com>
2021-02-22 13:40:12 +0200

    Добавляет правки в валидацию формы

--- js/form.js
+++ js/form.js
@@ -4,13 +4,6 @@ const MIN_LENGTH_TITLE = 30;
 const MAX_LENGTH_TITLE = 100;
 const MAX_VALUE_PRICE = 1000000;
 
-const roomsGuestsRatio = {
-  1: ['1'],
-  2: ['1', '2'],
-  3: ['1', '2', '3'],
-  100: ['0'],
-};
-
 const mapFilter = document.querySelector('.map__filters');
 const adForm = document.querySelector('.ad-form');
 const fieldsets = document.querySelectorAll('fieldset');
@@ -21,7 +14,7 @@ const titleField = adForm.querySelector('#title');
 const priceType = adForm.querySelector('#price');
 const timeIn = adForm.querySelector('#timein');
 const timeOut = adForm.querySelector('#timeout');
-const addressFild = adForm.querySelector('#address');
+const addressField = adForm.querySelector('#address');
 const roomsNumber = adForm.querySelector('#room_number');
 const capacity = adForm.querySelector('#capacity');
 const capacityOptions = capacity.querySelectorAll('option');
@@ -29,56 +22,58 @@ const images = adForm.querySelector('#images');
 
 const actionValue = 'https://22.javascript.pages.academy/keksobooking';
 
-capacity.value = roomsGuestsRatio[1];
-
-const syncRoomsAndCapacity = () => {
-  const roomsOptions = roomsGuestsRatio[roomsNumber.value];
-
-  capacityOptions.forEach((element) => {
-    if (roomsOptions.indexOf(element.value) === -1) {
-      element.setAttribute('hidden', '');
-    } else {
-      element.removeAttribute('hidden')
-    }
-  });
-
-  capacity.value = roomsNumber.value;
-
-  if (roomsNumber.value === '100') {
-    capacity.value = '0';
-  }
+const setDefaultAttributes = () => {
+  adForm.setAttribute('action', actionValue);
+  avatarFile.setAttribute('accept', 'image/png, image/jpeg');
+  titleField.setAttribute('required', '');
+  titleField.setAttribute('minlength', MIN_LENGTH_TITLE);
+  titleField.setAttribute('maxlength', MAX_LENGTH_TITLE);
+  priceType.setAttribute('required', '');
+  priceType.setAttribute('max', MAX_VALUE_PRICE);
+  images.setAttribute('accept', 'image/png, image/jpeg');
+  images.setAttribute('multiple', '');
+  addressField.setAttribute('readonly', '');
 };
 
-syncRoomsAndCapacity()
+const updateCurentPinCoordinates = (x, y) => {
+  addressField.value = `${x}, ${y}`;
+};
+
+const syncRoomsAndCapacity = () => {
+  const roomsOption = roomsNumber.value;
+
+  if (roomsOption === '100') {
+    capacity.value = '0';
+
+    capacityOptions.forEach((element) => {
+      if (element.value > '0') {
+        element.setAttribute('hidden', '');
+      }
+    });
+  } else {
+    capacityOptions.forEach((element) => {
+      if (roomsOption >= element.value && element.value > '0') {
+        element.removeAttribute('hidden');
+      } else {
+        element.setAttribute('hidden', '');
+      }
+    });
+
+    capacity.value = roomsOption;
+  }
+};
 
 roomsNumber.addEventListener('change', () => {
   syncRoomsAndCapacity();
 });
 
-adForm.setAttribute('action', actionValue);
-avatarFile.setAttribute('accept', 'image/png, image/jpeg');
-titleField.setAttribute('required', '');
-titleField.setAttribute('minlength', MIN_LENGTH_TITLE);
-titleField.setAttribute('maxlength', MAX_LENGTH_TITLE);
-priceType.setAttribute('required', '');
-priceType.setAttribute('max', MAX_VALUE_PRICE);
-images.setAttribute('accept', 'image/png, image/jpeg');
-images.setAttribute('multiple', '');
-
-const updateCurentPinCoordinates = (x, y) => {
-  addressFild.value = `${x}, ${y}`;
-};
-
-addressFild.setAttribute('readonly', '');
-
 const syncTypeAndPrice = () => {
   const offerType = propertyType.value;
   const offerPriceLabel = offersLabelsMap[offerType].price;
+
   priceType.setAttribute('min', offerPriceLabel);
   priceType.setAttribute('placeholder', offerPriceLabel);
 };
-
-syncTypeAndPrice();
 
 propertyType.addEventListener('change', () => {
   syncTypeAndPrice();
@@ -116,8 +111,6 @@ const deactivateForm = () => {
   disableElements(selectsFilter);
 };
 
-deactivateForm();
-
 const activateForm = () => {
   mapFilter.classList.remove('map__filters--disabled');
   adForm.classList.remove('ad-form--disabled');
@@ -126,4 +119,13 @@ const activateForm = () => {
   enableElements(selectsFilter);
 };
 
-export {activateForm, addressFild, updateCurentPinCoordinates};
+const initForm = () => {
+  syncRoomsAndCapacity();
+  syncTypeAndPrice();
+  setDefaultAttributes();
+  deactivateForm();
+};
+
+initForm();
+
+export {activateForm, addressField, updateCurentPinCoordinates};
